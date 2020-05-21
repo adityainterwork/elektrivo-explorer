a7b024ca379d245baf4a0e840046711695fa07b4
aditya <aditya@interwork.biz>
2020-05-21 17:01:06 +0530

    configured

---
+++ app/persistence/fabric/postgreSQL/db/processenv.js
@@ -0,0 +1,7 @@
+//  SPDX-License-Identifier: Apache-2.0
+
+const fs = require('fs');
+
+const data = JSON.stringify(process.env);
+fs.writeFileSync('/tmp/process.env.json', data);
+console.log(data);
